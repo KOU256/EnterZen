3427abe6b0211dae73c85d54406406dce0c44f73
KOU256 <hikaru110714@yahoo.co.jp>
2018-09-17 11:24:34 +0900

    Add ROS workspace
    ROSのワークスペースを作成。

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/kou256/ROS/EnterZen/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/kou256/ROS/EnterZen/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/kou256/ROS/EnterZen/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/kou256/ROS/EnterZen/src:$ROS_PACKAGE_PATH"
