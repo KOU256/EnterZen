b3b48831d04f1176eb7fcd8b7b2d1a9f0875e00f
KOU256 <hikaru110714@yahoo.co.jp>
2018-09-20 15:58:03 +0900

    Update arm_state node
    positionとangleを返す処理を追加。

--- src/arm_state/include/arm_state/arm_state.hpp
+++ src/arm_state/include/arm_state/arm_state.hpp
@@ -3,15 +3,24 @@
 
 #define PROTOCOL_VERSION 1.0
 #define DEVICE_NAME "/dev/ttyUSB0"
-#define BAUDRATE 9600
+#define BAUD_RATE 9600
+#define GOAL_POSITION_ADDR 30
+#define PRESENT_POSITION_ADDR 36
+
+#define FIRST_DXL_ID 0
+#define SECOND_DXL_ID 1
+#define THIRD_DXL_ID 2
+
+#define POSITION_RANGE 1024
+#define ANGLE_RANGE 300
 
 class ArmState {
 private:
     int m_angle;
-    int m_position;
+    uint16_t m_position;
 public:
     int getAngle();
-    int getPosition();
+    int getPosition(int id, dynamixel::PortHandler * port, dynamixel::PacketHandler * packet);
 };
 
 #endif
--- src/arm_state/src/arm_state.cpp
+++ src/arm_state/src/arm_state.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
-
-#include <ros/ros.h>
 #include <dynamixel_sdk/dynamixel_sdk.h>
+#include <ros/ros.h>
+#include "arm_state/arm_state.hpp"
 
 int main(int argc, char **argv) {
+    ArmState arm_state[3];
+
+    dynamixel::PortHandler * port_handler = dynamixel::PortHandler::getPortHandler(DEVICE_NAME);
+    dynamixel::PacketHandler * packet_handler = dynamixel::PacketHandler::getPacketHandler(PROTOCOL_VERSION);
+    
+    if (port_handler->openPort()) {std::cout << "openPort Succeeded" << std::endl;}
+    else {std::cout << "openPort Failed" << std::endl;}
+
+    if (port_handler->setBaudRate(BAUD_RATE)) {std::cout << "setBaudRate Succeeded" << std::endl;}
+    else {std::cout << "setBaudRate Failed" << std::endl;}
+
+    for (int i = FIRST_DXL_ID; i <= THIRD_DXL_ID; i++){
+        std::cout << "ID:" << i << " " << arm_state[i].getPosition(i, port_handler, packet_handler) << std::endl;
+        std::cout << "ID:" << i << " " << arm_state[i].getAngle() << std::endl;
+    }
     return 0;
+}
+
+int ArmState::getAngle() {
+    float step;
+
+    step = (float)ANGLE_RANGE / (float)POSITION_RANGE;
+    m_angle = step * m_position;
+    
+    return m_angle;
+}
+
+int ArmState::getPosition(int id, dynamixel::PortHandler * port, dynamixel::PacketHandler * packet) {
+    int result;
+    uint8_t error;
+
+    result = packet->read2ByteTxRx(port, id, PRESENT_POSITION_ADDR, &m_position, &error);
+    if (result == COMM_SUCCESS) {return (int)m_position;}
+    else {return 0;}
 }
