574356ed22f1e7726c7701ef6f71c2338ed78aef
KOU256 <hikaru110714@yahoo.co.jp>
2018-09-22 11:16:31 +0900

    Update arm_operate node
    Dynamixelを動かすための処理を追加。

--- src/arm_operate/src/arm_operate.cpp
+++ src/arm_operate/src/arm_operate.cpp
@@ -4,5 +4,31 @@
 #include "arm_operate/arm_operate.hpp"
 
 int main(int argc, char ** argv) {
+    dynamixel::PortHandler * port_handler =
+        dynamixel::PortHandler::getPortHandler(DEVICE_NAME);            //PortHandllerクラスのインスタンス
+    dynamixel::PacketHandler * packet_handler =
+        dynamixel::PacketHandler::getPacketHandler(PROTOCOL_VERSION);   //PacketHandlerクラスのインスタンス
+    int goal_position;
+    ArmOperate arm_operate;
+    port_handler->openPort();
+	port_handler->setBaudRate(BAUD_RATE);
+
+    std::cin >> goal_position;
+    arm_operate.setGoalPosition(FIRST_DXL_ID, goal_position, port_handler, packet_handler);
+
     return 0;
 }
+
+int ArmOperate::setGoalPosition(
+    int id,
+    int goal_position,
+    dynamixel::PortHandler * port,
+    dynamixel::PacketHandler * packet
+) {
+    int result;
+    uint8_t error;
+
+    result = packet->write2ByteTxRx(port, id, GOAL_POSITION_ADDR, goal_position, &error);
+
+    return result;
+};
