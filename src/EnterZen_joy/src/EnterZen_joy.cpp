d7278b2e783af101adaa5f05478534de8b6c5969
KOU256 <g15219@g.ichinoseki.ac.jp>
2018-10-10 17:21:57 +0900

    Update EnterZen_joy.cpp
    コメント文の追加や改行などを行った。

--- src/EnterZen_joy/src/EnterZen_joy.cpp
+++ src/EnterZen_joy/src/EnterZen_joy.cpp
@@ -1,8 +1,10 @@
-#include<ros/ros.h>
-#include<sensor_msgs/Joy.h>
-#include<std_msgs/Float32MultiArray.h>
-#include"EnterZen_joy/EnterZen_joy.hpp"
+/* ヘッダファイルのインクルード */
+#include <ros/ros.h>
+#include <sensor_msgs/Joy.h>
+#include <std_msgs/Float32MultiArray.h>
+#include "EnterZen_joy/EnterZen_joy.hpp"
 
+/* joyトピックからメッセージを受け取ったときに呼び出される関数 */
 void joyCallback(const sensor_msgs::Joy::ConstPtr joy) {
     std_msgs::Float32MultiArray axes;
     ros::NodeHandle nh;
@@ -10,18 +12,21 @@ void joyCallback(const sensor_msgs::Joy::ConstPtr joy) {
 
     axes.data.resize(2);
     ros::Rate loop_rate(10);
+
     while(ros::ok()) {
         for(EAxes index : {LEFT_STICK_HOR, LEFT_STICK_VER}){
             if(joy->axes[index] > 0.3 || joy->axes[index] < -0.3){
                 axes.data[index] = joy->axes[index];
             }
         }
+        
         joy_pub.publish(axes);
         ros::spinOnce();
         loop_rate.sleep();
     }
 }
 
+/* メイン関数 */
 int main(int argc, char** argv) {
     ros::init(argc, argv, "EnterZen_joy");
     ros::NodeHandle nh;
